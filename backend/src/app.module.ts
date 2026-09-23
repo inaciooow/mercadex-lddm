db05c8bed4e39ba5b2b9d3d53ab9474923c7f999
VLtim43 <fernandoreal.valentim@gmail.com>
2026-09-23 19:14:17 -0300

    seed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
